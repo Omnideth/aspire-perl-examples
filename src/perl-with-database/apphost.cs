31d297cd5b860cb2ac4a38b87e21f934d5ce091d
Omnideth <omnideth@gmail.com>
2026-03-15 16:10:18 -0500

    shipping up repo inits

---
+++ src/perl-with-database/apphost.cs
@@ -0,0 +1,7 @@
+﻿#:package Aspire.Hosting.PostgreSQL@13.3.0-preview.1.26163.4
+#:sdk Aspire.AppHost.Sdk@13.2.0-preview.1.26163.12
+#:project ../../../CommunityToolkit.Aspire.Hosting.Perl/src/CommunityToolkit.Aspire.Hosting.Perl
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+builder.Build().Run();
